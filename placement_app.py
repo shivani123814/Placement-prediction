f11375eb808e5785dd3e1831ba232c489d158be5
Shivani Meena <shivimeena167@gmail.com>
2026-05-03 16:18:36 +0530

    Update placement_app.py

--- placement_app.py
+++ placement_app.py
@@ -5,27 +5,30 @@ import numpy as np
 # Load trained model
 model = pickle.load(open('placement.pkl', 'rb'))
 
-# Page title
-st.set_page_config(page_title="Placement Prediction System")
+# Page settings
+st.set_page_config(page_title="Package Prediction App")
 
-st.title("🎓 Placement Prediction System")
-st.write("Enter student details to predict placement status")
+# Title
+st.title("🎓 Student Package Prediction System")
 
-# Input fields
-cgpa = st.number_input("Enter CGPA", min_value=0.0, max_value=10.0, step=0.1)
-iq = st.number_input("Enter IQ", min_value=0, max_value=200, step=1)
-profile_score = st.number_input("Enter Profile Score", min_value=0, max_value=100, step=1)
+st.write("Enter CGPA to predict expected placement package")
 
-# Prediction button
-if st.button("Predict Placement"):
-     # Create input array
-    input_data = np.array([[cgpa, iq, profile_score]])
-    
-    # Predict
+# Input
+cgpa = st.number_input(
+    "Enter CGPA",
+    min_value=0.0,
+    max_value=10.0,
+    step=0.1
+)
+
+# Prediction
+if st.button("Predict Package"):
+
+    # Convert input into array
+    input_data = np.array([[cgpa]])
+
+    # Predict package
     prediction = model.predict(input_data)
-    
-    # Output result
-    if prediction[0] == 1:
-        st.success("✅ Student is Likely to be Placed")
-    else:
-        st.error("❌ Student is Not Likely to be Placed")
+
+    # Show output
+    st.success(f"Expected Package: {prediction[0]:.2f} LPA")
